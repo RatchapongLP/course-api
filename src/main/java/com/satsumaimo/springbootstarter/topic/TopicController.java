93c6176d3597effe404211a242fc172cbf112dfa
RatchapongLP <ratchapong.lpz@gmail.com>
2025-10-16 11:55:54 +0700

    Add get a topic, add a topic api and refactor to use TopicService

--- src/main/java/com/satsumaimo/springbootstarter/topic/TopicController.java
+++ src/main/java/com/satsumaimo/springbootstarter/topic/TopicController.java
@@ -1,21 +1,40 @@
 package com.satsumaimo.springbootstarter.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
+	
+	@Autowired
+	private TopicService topicService;
 
 	// Spring MVC auto-detect the Java Bean (List) as response and convert it to JSON
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("spring", "Spring Framework", "Spring Framework Description"),
-				new Topic("java", "Core Java", "Core Java Description"),
-				new Topic("javascript", "JavaScript", "JavaScript Description")
-				);
+		return topicService.getAllTopic();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	
+	// For unmatched path variable and parameter name
+//	@RequestMapping("/topics/{courseId}")
+//	public Topic getTopicLong(@PathVariable("courseId") String id) {
+//		return topicService.getTopic(id);
+//	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
 	}
 }
