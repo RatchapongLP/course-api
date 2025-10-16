92fc828e48196b3e6f832c6af8b383bb2ce47841
RatchapongLP <ratchapong.lpz@gmail.com>
2025-10-16 14:41:00 +0700

    Add update and delete Topic apis

--- src/main/java/com/satsumaimo/springbootstarter/topic/TopicService.java
+++ src/main/java/com/satsumaimo/springbootstarter/topic/TopicService.java
@@ -28,4 +28,17 @@ public class TopicService {
 	public void addTopic(Topic topic) {
 		topics.add(topic);		
 	}
+
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			if (topics.get(i).getId().equals(id) ) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
 }
